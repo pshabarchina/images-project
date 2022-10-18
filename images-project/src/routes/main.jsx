47e55d8ef7bf8fac4e7146d5826bc8ae6d1756eb
Polina Shabarchina <polina@MacBook-Air-Polina.local>
2022-10-18 14:30:43 +0500

    show correct color for like buttons

--- images-project/src/routes/main.jsx
+++ images-project/src/routes/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useLocation, Link } from 'react-router-dom';
 
 export default function Main(props) {
   
   console.log(props);
   function handleLikeClick(e) {
     let spanEl = e.target.closest('.imgSpan');
-    e.target.style.backgroundColor = "gray";
+    e.target.classList.remove('likeBtn');
+    e.target.classList.add('likeBtn-clicked');
     if (!props.likedImgs.includes(spanEl.id)) 
       props.addLikedImage(spanEl.id);
-    console.log(props.likedImgs);
+    //console.log(props.likedImgs);
   }
 
-  const images = props.data.map(entry => (<p className='imgSpan' id={entry.url}><img src={entry.url} height='200' /><button className='likeBtn' onClick={handleLikeClick}>Like</button></p>));
-    
+  const images = props.data.map(entry => (<p className='imgSpan' id={entry.url}><img src={entry.url} height='200' /><button className={props.likedImgs.includes(entry.url)?"likeBtn-clicked":"likeBtn"} onClick={handleLikeClick}>Like</button></p>));
+
   return (
     <main>
       <h2>Main</h2>
